632603544ba178255133d1a8ea877bd8effc478a
Typhoon0x73 <43872843+Typhoon0x73@users.noreply.github.com>
2025-11-19 08:30:07 +0900

    Create Background.h

---
+++ Background/Background.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "../DrawObject/DrawObject.h"
+
+namespace stn
+{
+	class Background : public DrawObject
+	{
+		using super = DrawObject;
+	public:
+
+		Background();
+		virtual ~Background();
+
+		virtual void draw() const override;
+	};
+}
