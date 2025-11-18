d6525688422be73c33af3bd8b267b3b538e6fe6d
Typhoon0x73 <43872843+Typhoon0x73@users.noreply.github.com>
2025-11-19 08:30:09 +0900

    Create Background.cpp

---
+++ Background/Background.cpp
@@ -0,0 +1,37 @@
+﻿#include "Background.h"
+#include "../ServiceLocator/SingletonLocator.h"
+
+namespace stn
+{
+
+	Background::Background()
+		: super{ Layer::Background }
+	{
+	}
+
+	Background::~Background()
+	{
+	}
+
+	void Background::draw() const
+	{
+		const auto* pCamera = STN_GET_LOCATOR(BasicCamera2D).get();
+		if (pCamera == nullptr)
+		{
+			return;
+		}
+		int32 space = (pCamera->getScale() < 0.3 ? 100 : 50);
+		const auto& region = pCamera->getRegion();
+		const double offsetX = region.x - static_cast<int32>(region.x) % space;
+		const double offsetY = region.y - static_cast<int32>(region.y) % space;
+		for (double y = offsetY; y < region.bottomY(); y += space)
+		{
+			for (double x = offsetX; x < region.rightX(); x += space)
+			{
+				Circle{ x, y, 3 }.draw(Palette::Darkgray);
+			}
+		};
+	}
+
+}
+
